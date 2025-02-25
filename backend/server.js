c1254dab4399a2ec16188ff9973b16b9bfe1564b
Ziad0120 <ziadm8445@gmail.com>
2025-02-25 21:34:14 +0200

    just update

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000; // استخدام متغير البيئة أو المنفذ 5000
+const PORT = process.env.PORT || 8386; // استخدام متغير البيئة أو المنفذ 5000
 
 // Middleware
 app.use(cors()); // السماح لجميع الطلبات من أي مصدر
